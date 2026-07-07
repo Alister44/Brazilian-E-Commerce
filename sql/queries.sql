c3ddc2e20899f33a22c5b3bd5ef1a6926af85bdb
ctorex <45258900+Alister44@users.noreply.github.com>
2026-07-07 14:56:24 +0200

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -16,7 +16,7 @@ left JOIN product_category_name_translation t USING (product_category)
 left join olist_order_payments_dataset op using(order_id)
 left join olist_order_reviews_dataset r USING(order_id)
 where o.order_status = 'delivered';
-
+------------------------------------------------------------------------
 --місячний виторг і кількість замовлень
 SELECT
 	  strftime('%Y-%m',o.order_purchase_t) as ym,
@@ -27,3 +27,20 @@ join olist_order_items_dataset oi using (order_id)
 where o.order_status = 'delivered'
 GROUP by ym
 order by ym;
+------------------------------------------------------------------------
+-- Розвідувальні запити
+-- •       топ-10 категорій за виторгом;
+-- •       виторг за штатами (для карти в Tableau);
+-- •       середня оцінка (review_score) за категоріями;
+-- •       середній час доставки (різниця між датою купівлі і датою доставки);
+-- •       розподіл способів оплати.
+select t.product_category_1 as category_en,
+	   round(sum(oi.price),2) as revenue
+from olist_order_items_dataset oi
+join olist_orders_dataset o using(order_id)
+join olist_products_dataset p using(product_id)
+left join product_category_name_translation t using (product_category)
+where o.order_status = 'delivered'
+GROUP by category_en
+order by revenue DESC
+limit 10;
